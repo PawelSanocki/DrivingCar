59fbf212468bf671deb230fd8295fc18c0633610
PawelSanocki <35131417+PawelSanocki@users.noreply.github.com>
2019-07-10 21:08:28 +0200

    fast one

--- DrivingCar/LinesDetecting/main.py
+++ DrivingCar/LinesDetecting/main.py
@@ -2,39 +2,59 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 from os.path import join, basename
-from _collections import deque
-from laneDetection import find_lanes
+from collections import deque
+from lane_detection import find_lanes
 
 
 if __name__ == '__main__':
 
     resize_h, resize_w = 540, 960
 
-    verbose = False
-    if verbose:
-        plt.ion()
-        figManager = plt.get_current_fig_manager()
-        figManager.window.showMaximized()
-
     # test on images
-    test_images_dir = join('LinesDetecting\\data', 'test_images')
-    # print(test_images_dir)
+    test_images_dir = join('1_lane_marking//data', 'test_images')
     test_images = [join(test_images_dir, name) for name in os.listdir(test_images_dir)]
-    # print(test_images)
+
     for test_img in test_images:
+
         print('Processing image: {}'.format(test_img))
-        
-        out_path = join('out', 'images', basename(test_img))
-        in_image = cv2.imread(test_img)
+
+        out_path = join('1_lane_marking//out', 'images', basename(test_img))
+        in_image = cv2.imread(test_img, cv2.IMREAD_COLOR)
         out_image = find_lanes(in_image)
-        # cv2.imwrite(out_path, cv2.cvtColor(out_image, cv2.COLOR_RGB2BGR))  
-        cv2.imshow('1',in_image)
-        cv2.imshow('2',out_image)
-        cv2.waitKey()    
-    if verbose:
-            cv2.imshow('1',in_image)
-            cv2.waitKey()
-            cv2.imshow('2',out_image)
-            cv2.waitKey()
-            plt.close('all')
+        cv2.imwrite(out_path, out_image)
+        
+        cv2.imshow('final',out_image)
+        cv2.waitKey()
     cv2.destroyAllWindows()
+
+    # # test on videos
+    # test_videos_dir = join('data', 'test_videos')
+    # test_videos = [join(test_videos_dir, name) for name in os.listdir(test_videos_dir)]
+
+    # for test_video in test_videos:
+
+    #     print('Processing video: {}'.format(test_video))
+
+    #     cap = cv2.VideoCapture(test_video)
+    #     out = cv2.VideoWriter(join('out', 'videos', basename(test_video)),
+    #                           fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
+    #                           fps=20.0, frameSize=(resize_w, resize_h))
+
+    #     frame_buffer = deque(maxlen=10)
+    #     while cap.isOpened():
+    #         ret, color_frame = cap.read()
+    #         if ret:
+    #             color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
+    #             color_frame = cv2.resize(color_frame, (resize_w, resize_h))
+    #             frame_buffer.append(color_frame)
+    #             blend_frame = color_frame_pipeline(frames=frame_buffer, solid_lines=True, temporal_smoothing=True)
+    #             out.write(cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR))
+    #             cv2.imshow('blend', cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR)), cv2.waitKey(1)
+    #         else:
+    #             break
+    #     cap.release()
+    #     out.release()
+        # cv2.destroyAllWindows()
+
+
+
